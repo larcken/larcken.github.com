a94eae04404389dae5a70de57e7cab3ee55cffdd
larcken <amps@naver.com>
2016-06-09 23:42:05 +0900

    image resize

--- resource/mcard.js
+++ resource/mcard.js
@@ -4,7 +4,7 @@ var amt = 10,m={};
 
 function src(){
 	var nm;
-	return nm = ((new Date).getMilliseconds() % amt + 1), (nm < 6 ? '/resource/images/0'+nm : './img/'+nm) + '.jpg';
+	return nm = ((new Date).getMilliseconds() % amt + 1), '/resource/images/0' + nm + '.jpg';
 }
 
 function gen(){
