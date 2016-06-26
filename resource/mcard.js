04d6676c332c1666271b620af253aaed2123bd11
larcken <amps@naver.com>
2016-06-27 02:07:24 +0900

    map test

--- resource/mcard.js
+++ resource/mcard.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+	$('#btnReplay').on('click',function(e){
+		e.preventDefault();
+		var name = $('#name').val(), message = $('#message').val();
+		if(name && message) {
+			fb.push({name:name,message:message});
+			$('#message').val('');
+		}
+	});
+
+	fb.on('child_added',function(s){
+		addReply(s.val());
+	})
+
+	setMap();
+});
+
 
 // reply
 var fb = new Firebase('https://larcken.firebaseio.com/'), num = 0;
@@ -12,17 +28,22 @@ function addReply(data){
 		$('#view').prepend(text);
 }
 
-$('#btnReplay').on('click',function(e){
-	e.preventDefault();
-	var name = $('#name').val(), message = $('#message').val();
-	if(name && message) {
-		fb.push({name:name,message:message});
-		$('#message').val('');
-	}
-});
+function scrollLink(obj){
+	 var position = $("#"+obj).offset();
+	 $('html, body').animate({scrollTop : position.top}, 1000);
+}
 
-fb.on('child_added',function(s){
-	addReply(s.val());
-})
+function setMap() {
+	//지도를 삽입할 HTML 엘리먼트 또는 HTML 엘리먼트의 id를 지정합니다.
+	var mapDiv = document.getElementById('map'); // 'map' 으로 선언해도 동일
 
-});
+	var map = new naver.maps.Map('map', {
+	    center: new naver.maps.LatLng(37.4501224, 127.1272767),
+	    zoom: 10
+	});
+
+	var marker = new naver.maps.Marker({
+	    position: new naver.maps.LatLng(37.4501224, 127.1272767),
+	    map: map
+	});
+}
